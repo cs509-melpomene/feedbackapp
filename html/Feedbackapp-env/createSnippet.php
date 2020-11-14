558e9810135ab5a4b788da0f47075c9520d8b7d6
Yahel Nachum <yahelnachum@yahoo.com>
2020-11-14 17:01:29 -0500

    Comment (#15)
    * Created comments form and area for comments
    
    filled in with dummy comments at the moment
    
    * Styling comment form a bit too
    
    * Bring similar start / end region styling
    
    to comments area
    
    * Added highlighting
    
    * Fixed highlighting, notfound, and failed create
    
    highlighiting
    - can directly click on next comment to view
    - scrolls with snippet
    
    notfound
    - not redirect anymore
    - clears snippet view
    - complains about snippet id directly
    
    failed create
    - not redirecting if create-snippet failed
    
    * Highlight comment side + resize highlight width
    
    * Moved createSnippet css to its own file
    
    * Moved createSnippet javascript to its own file
    
    * Masking highlight on the top
    
    * Pulled in Halie's line numbers
    
    + updated line numbers on view or update snippet

--- html/Feedbackapp-env/createSnippet.php
+++ html/Feedbackapp-env/createSnippet.php
@@ -5,6 +5,7 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
 	<link rel="stylesheet" href="mainGrid.css"/>
 	<link rel="icon" type="image/png" href="favicon.png"/>
+	<link rel="stylesheet" href="createSnippet.css" />
 	<title>Feedback Application</title>
 </head>
 
@@ -12,7 +13,7 @@
     <form>
 	<div class="container-fluid">
 		<div class="row" >
-			<div class="col-md-3" style="border-style: solid;">
+			<div class="col-md-3" id="snippetInfoSidePanel" style="border-style: solid;">
 				<br>
 					Unique ID: <?php echo($_GET["snippetID"])?><br>
 					<a href="http://feedbackapp-env.eba-9ipq52ps.us-east-2.elasticbeanstalk.com/createSnippet.php?snippetID=<?php echo($_GET["snippetID"])?>" target="_blank">Click to open Viewer Screen</a>
@@ -26,92 +27,55 @@
                     ?>><br>
 					<div>
                     </div>
-					<label>Information:</label<br><textarea rows="20" cols="30" id="info"
+					<label>Information:</label><br><textarea rows="15" cols="30" id="info"
 					    <?php
                             if (strcmp($_POST["isCreator"] , "true" ) != 0){
                               echo("readonly");
                            }
                         ?>></textarea><br>
 					<div>
-                    </div>
-                    <input type="button" onclick="deleteSnippet()" value="Delete Snippet" style = "font-size:20px">
+					</div>
+					<?php if (strcmp($_POST["isCreator"] , "true" ) == 0): ?>
+                        <input type="button" onclick="deleteSnippet()" value="Delete Snippet" style = "font-size:20px">
+                    <?php endif; ?>
 
 			</div>
 			<div class="col-md-1" style="border-style: solid none solid none; text-align: right;">
+				<br>
+				<br>
+				<textarea readonly id="numbers" class="snippetLineNumbers numbers" cols="4" rows="27"><?php for($i = 1; $i <= 500; $i++){echo $i; echo ("\n");}?></textarea>
 			</div>
-			<div class="col-md-5" style="border-style: solid none solid none;">
+			<div class="col-md-5" id="snippetTextPanel" style="border-style: solid none solid none;">
+				<div class="highlightWrapper" id="highlightWrapper">
+					<div class="highlightMaskTop" id="highlightMaskTop"></div>
+				</div>
 				<br>
-				<label>Code: </label><br><textarea id="text" oninput="codeFunction()" rows="27" cols="68"></textarea><br>
+				<label>Code: </label><br>
+				<div class="highlightWrapper" id="highlightWrapper">
+					<div class="highlight" id="highlight"></div>
+				</div>
+				<textarea class="snippetText" id="text" oninput="codeFunction()" onscroll="codeScrolling()"></textarea>
+				<br>
 			</div>
-			<div class="col-md-3" style="border-style: solid;">
+			<div class="commentSidebar" id="commentSidebar" style="border-style: solid;">
 				<br>
-				<label>Comments: </label><br><textarea rows="27" cols="30" id="comment"></textarea><br>
-				<br>
-				<br>
+				<label>Comments: </label><br>
+				<div class="singleComment">
+				<form action="">
+					<label class="commentFormColumnLabel" for="regionStart">Start Region:</label>
+					<input class="commentFormColumnInput" type="text" id="regionStartID" name="regionStart">
+					<label class="commentFormColumnLabel" for="regionEnd">End Region:</label>
+					<input class="commentFormColumnInput" type="text" id="regionEndID" name="regionEnd">
+					<textarea class="columnFormText" id="comment"></textarea><br>
+					<input class="submitColumnButton" type="submit" formmethod="post" onclick="submitComment()"/><br>
+				</form>
+				</div>
+				<div id="comments">
+				</div>
 			</div>
 		</div>
 	</div>
 	</form>
 </body>
-<script>
-function deleteSnippet(){
-
-}
-function codeFunction() {
-  httpRequest = new XMLHttpRequest();
-  httpRequest.open('POST', 'https://pg407hi45l.execute-api.us-east-2.amazonaws.com/beta/snippet/<?php echo($_GET["snippetID"])?>', true);
-  httpRequest.setRequestHeader('Content-Type', 'application/json');
-  let body = document.getElementById("text").value;
-  body = body.replace(/\n/g,"\\n").replace(/\r/g,"\\r").replace(/\"/g,'\\"')
-  body = '{"action":"update","text":"' + body + '"}';
-  console.log(body);
-  httpRequest.send(body);
-  httpRequest.onreadystatechange = nameOfTheFunction;
-}
-
-  httpRequest1 = new XMLHttpRequest();
-  httpRequest1.open('GET', 'https://pg407hi45l.execute-api.us-east-2.amazonaws.com/beta/snippet/<?php echo($_GET["snippetID"])?>', true);
-  httpRequest1.setRequestHeader('Content-Type', 'application/json');
-  httpRequest1.send();
-  httpRequest1.onreadystatechange = nameOfTheFunction2;
-
-function nameOfTheFunction() {
-  if (httpRequest.readyState === XMLHttpRequest.DONE) {
-      if (httpRequest.status === 200) {
-		//console.log("responseText: " + httpRequest.responseText)
-		//const obj = JSON.parse(httpRequest.responseText);
-		console.log("success")
-      } else {
-        console.log("status: " + httpRequest.status)
-      }
-
-  } else {
-	  console.log("not done")
-  }
-}
-function nameOfTheFunction2() {
-  if (httpRequest1.readyState === XMLHttpRequest.DONE) {
-      if (httpRequest1.status === 200) {
-        let body1 = '{"action":"update","text":"' + document.getElementById("text").value + '"}'
-		console.log("responseText: " + httpRequest1.responseText)
-		const obj = JSON.parse(httpRequest1.responseText);
-		if (obj['snippet'] === undefined) {
-			window.location.href = ('./notfound.php');
-			return;
-		}
-		
-		document.getElementById("text").value = obj['snippet']['text'];
-		document.getElementById("timestampDiv").innerHTML = obj['snippet']['timestamp'];
-		document.getElementById("Planguage").value = obj['snippet']['codingLanguage'];
-		
-		console.log("success")
-      } else {
-        console.log("status: " + httpRequest1.status)
-      }
-
-  } else {
-	  console.log("bad")
-  }
-}
-</script>
+<script src="createSnippet.js"></script>
 </html>
