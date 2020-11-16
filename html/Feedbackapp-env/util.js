9aeed973d0ae9ccc37620e2f4c3b71bb5c9a1aa3
Yahel Nachum <yahelnachum@yahoo.com>
2020-11-16 17:48:43 -0500

    Making region start / end automatic
    based on selection

--- html/Feedbackapp-env/util.js
+++ html/Feedbackapp-env/util.js
@@ -4,9 +4,9 @@ console.log(urlParams.get('snippetID')); // true
 export const urlParamsSnippetID = urlParams.get('snippetID');
 
 export function updateNumbers(text){
-    let numOfNumbers = text.replace(/[^\n]/g,"").length;
+    let numOfNumbers = text.split("\n").length;
     let numbersStr = "";
-    for (let i = 1; i <= numOfNumbers + 1; i++){ // adding 1 because last line might not have newline char
+    for (let i = 1; i <= numOfNumbers; i++){
         numbersStr += i + "\n"
     }
     document.getElementById("numbers").value = numbersStr;
