0a4f42f8363bdd176ce86c1d84b1eb3cd4b2b8d8
Yahel Nachum <yahelnachum@yahoo.com>
2020-12-06 17:49:18 -0500

    UI admin (#41)
    * Add admin to a new branch 
    
    Created a new branch and added new admin page to it
    
    * Update Admin.php
    
    * Delete Admin.php
    
    * uploaded and updated scripts
    
    uploaded viewAllSnippets.js
    updated Admin.php
    updated deleteSnippet.js
    
    Co-authored-by: haliemartineau <71106707+haliemartineau@users.noreply.github.com>

---
+++ html/Feedbackapp-env/viewAllSnippets.js
@@ -0,0 +1,46 @@
+import { deleteSnippetHTTPRequest } from './deleteSnippet.js';
+
+window.deleteSnippetHTTPRequest = deleteSnippetHTTPRequest
+window.viewAllSnippetsHTTPRequest = viewAllSnippetsHTTPRequest
+
+viewAllSnippetsHTTPRequest()
+console.log("Hello")
+export function viewAllSnippetsHTTPRequest(){
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.open('GET', `https://pg407hi45l.execute-api.us-east-2.amazonaws.com/beta/snippets`, true);
+    httpRequest.setRequestHeader('Content-Type', 'application/json');
+    httpRequest.send();
+    httpRequest.onreadystatechange = viewAllSnippetsHTTPResponse(httpRequest);
+}
+
+function viewAllSnippetsHTTPResponse(httpRequest){
+    return function(){
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            if (httpRequest.status === 200) {
+                console.log("responseText: " + httpRequest.responseText)
+                let viewAllSnippets = JSON.parse(httpRequest.responseText).snippets
+                for (let i = 0; i <= viewAllSnippets.length; i++){
+
+                    let SnippetID = viewAllSnippets[i].substring(0, 16)
+                    let time = viewAllSnippets[i].substring(16)
+                    let tableShow = `<tr>
+                                             <td style="border-style:none solid  none none;">${SnippetID}</td>
+                                            <td>${time}</td>
+                                     </tr>`
+                    let tableDelete = `<tr>
+                                <td style="border-style:none solid  none none;">${SnippetID}</td>
+                                <td style="border-style:none solid  none none;">${time}</td>
+                                <td><input type="button" value="Delete Snippet" style="text-align: center; width: 100%;" onclick='deleteSnippetHTTPRequest("${SnippetID}")'></td>
+                         </tr>`
+                    let deleteTable = document.getElementById('DeleteTable')
+                    let showTable = document.getElementById('ShowTable')
+                    deleteTable.innerHTML += tableDelete
+                    showTable.innerHTML += tableShow
+                }
+                return;
+            }
+        } else {
+            console.log("bad")
+        }
+    }
+}
